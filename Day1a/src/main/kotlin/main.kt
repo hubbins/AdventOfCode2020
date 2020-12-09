27027160cdc025e2af45e0ce40a97e89ba848eae
Sean O'Connor <soconnor@yahoo.com>
2020-12-09 16:35:36 -0600

    Initial commit

---
+++ Day1a/src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    val dataSet = getData("input.txt")
+
+    val item = dataSet.find { dataSet.contains(2020 - it) }
+    if (item != null) {
+        val otherItem = 2020 - item
+        println("${item} + ${otherItem} = ${item * otherItem}")
+    }
+}
+
+fun getData(fileName: String): HashSet<Int> {
+    val inputSet = HashSet<Int>()
+
+    File(ClassLoader.getSystemResource("input.txt").file)
+        .readLines()
+        .forEach {
+            if (it.trim().length > 0) {
+                inputSet.add(it.toInt())
+            }
+        }
+
+    return inputSet
+}
